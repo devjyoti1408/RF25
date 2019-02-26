0d53a8af97503526d86395ab125ac50a954c664a
Dev <Dev@DevsLap>
2019-02-26 19:22:12 +0530

    testng updated

--- GitHub_Repo1/src/git/Script1.java
+++ GitHub_Repo1/src/git/Script1.java
@@ -1,5 +1,32 @@
 package git;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Script1 {
 
+	WebDriver driver;
+	@BeforeMethod
+	public void setUP()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void openBrowser()
+	{
+		driver.get("http://localhost/login.do");
+	}
+	
+	@AfterMethod
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
 }
